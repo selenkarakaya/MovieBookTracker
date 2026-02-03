3d144c32b548921edff47d906489d5110b3e35d0
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2026-02-03 12:37:07 +0000

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,15 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 
-const firebaseConfig = {
-  apiKey: "your_key",
-  authDomain: "your_domain.firebaseapp.com",
-  projectId: "project_name",
-  storageBucket: "project_name.appspot.com",
-  messagingSenderId: "your_id",
-  appId: "your_appID",
-};
 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 // Initialize Firebase
 initializeApp(firebaseConfig);
 export const db = getFirestore();
