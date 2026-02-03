90c6c17872c8b7b3480adc513f1335b19d0237b2
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2026-02-03 12:39:15 +0000

    Add Firebase Auth initialization to config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,8 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,6 +10,8 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
-// Initialize Firebase
-initializeApp(firebaseConfig);
-export const db = getFirestore();
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
